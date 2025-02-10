6ac67a36332754e69719a91c858abde928c14aef
AayushKarwa <ayushkarwa18@gmail.com>
2025-02-10 12:29:38 +0530

    initialized a t3

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "~/components/ui/button";
+
+
+export default async function Home() {
+  
+
+  return (
+    <div>
+      <Button className="bg-red-600"> Hello Folks!</Button>
+    </div>
+  );
+}
